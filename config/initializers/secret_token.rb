d243cda9258a4fbfa7129c940d6ec2673191e015
ed wassermann <vis0vis@gmail.com>
2013-02-10 18:35:36 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Triennale::Application.config.secret_token = 'db19c391a8635133c418506ae735e08ec94076102c65f82fae06503d850190ced32ab5f67cc09699827ed01d36642177352a96a477fefd8825c3195679e86ba8'
